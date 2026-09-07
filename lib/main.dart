f0459d286e87e105135287fe26d692b5edb094be
tavakkoly1399-cloud <tavakkoly1399@gmail.com>
2026-09-07 20:18:27 +0330

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1098,23 +1098,16 @@ class _HomePageState extends State<HomePage>
       'dns': {
         'servers': [
           {
-            'type': 'https',
             'tag': 'dns-remote',
-            'server': '1.1.1.1',
-            'server_port': 443,
-            'path': '/dns-query',
-            'tls': {
-              'enabled': true,
-              'server_name': 'cloudflare-dns.com',
-            },
+            'address': 'https://1.1.1.1/dns-query',
+            'detour': 'proxy',
           },
           {
-            'type': 'local',
             'tag': 'dns-direct',
+            'address': 'local',
           },
         ],
         'final': 'dns-remote',
-        'strategy': 'prefer_ipv4',
       },
 
       'inbounds': [
@@ -1132,6 +1125,7 @@ class _HomePageState extends State<HomePage>
           // Force Android's VPN/TUN routing path to handle all routed
           // traffic and help prevent traffic/DNS leaks when auto_route
           // is enabled.
+          'strict_route': true,
 
           // mixed is better for normal TCP + UDP apps
           // such as Telegram and other mobile applications.
